fe5cb6fb593ad86fbbba97296ade6903c5040a3a
BernardKazazic <92548057+BernardKazazic@users.noreply.github.com>
2023-05-20 14:42:36 +0200

    initialized spring project

---
+++ fit-backend/src/main/java/fer/infsus/fitnessify/FitnessifyApplication.java
@@ -0,0 +1,13 @@
+package fer.infsus.fitnessify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessifyApplication.class, args);
+	}
+
+}
---
+++ fit-backend/src/test/java/fer/infsus/fitnessify/FitnessifyApplicationTests.java
@@ -0,0 +1,13 @@
+package fer.infsus.fitnessify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
