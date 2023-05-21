ac8af666f20182a77d4451d4be79a0b1b67b0ac6
BernardKazazic <92548057+BernardKazazic@users.noreply.github.com>
2023-05-21 23:36:56 +0200

    tests finn

--- fit-backend/src/test/java/fer/infsus/fitnessify/AbstractTestIT.java
+++ fit-backend/src/test/java/fer/infsus/fitnessify/AbstractTestIT.java
@@ -1,4 +1,15 @@
 package fer.infsus.fitnessify;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AbstractTestIT {
+    @BeforeEach
+    void clearDatabase(@Autowired Flyway flyway) {
+        flyway.clean();
+        flyway.migrate();
+    }
 }
--- fit-backend/src/test/java/fer/infsus/fitnessify/integration/ExerciseTestIT.java
+++ fit-backend/src/test/java/fer/infsus/fitnessify/integration/ExerciseTestIT.java
@@ -1,4 +1,10 @@
 package fer.infsus.fitnessify.integration;
 
-public class ExerciseTestIT {
+import fer.infsus.fitnessify.AbstractTestIT;
+import fer.infsus.fitnessify.interfaces.controller.ExerciseController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ExerciseTestIT extends AbstractTestIT {
+    @Autowired
+    private ExerciseController controller;
 }
