bce0c7f808f17cf04775209d0e1dd708bc8fee8a
mateagluhak <mg51820@fer.hr>
2023-05-21 13:04:30 +0200

    UI: Adding exercises and removing them from workout

--- fit-ui/src/pages/EditWorkout.tsx
+++ fit-ui/src/pages/EditWorkout.tsx
@@ -1,42 +1,150 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaEdit } from 'react-icons/fa';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/'
+});
+
+interface Exercise {
+  id: number;
+  name: string;
+  maxWeight: number;
+}
+
+interface Workout {
+  id: number;
+  name: string;
+  workoutPlanId: number;
+  exerciseIds: number[];
+}
 
 function EditWorkout() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-
-  // Sample data for exercises
-  const [exercises, setExercises] = useState([
-    { id: 1, name: 'Exercise 1', maxWeight: 100 },
-    { id: 2, name: 'Exercise 2', maxWeight: 150 },
-    { id: 3, name: 'Exercise 3', maxWeight: 120 },
-  ]);
-
-  const [selectedExercise, setSelectedExercise] = useState<null | { id: number; name: string; maxWeight: number }>(null);
-  const [workoutName, setWorkoutName] = useState(`Workout ${id}`);
+  const { id } = useParams<{ id: string }>();
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+  const [allExercises, setAllExercises] = useState<Exercise[]>([]); // Updated state for all exercises
+  const [workout, setWorkout] = useState<Workout>();
+  const [workoutName, setWorkoutName] = useState('');
   const [isEditing, setIsEditing] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedExerciseId, setSelectedExerciseId] = useState<number | null>(null);
 
-  // Handle deleting an exercise
-  const handleDelete = (exerciseId: number) => {
-    // Implement delete logic here
+  const fetchWorkout = () => {
+    api.get<Workout>(`/workout/${id}`).then((response) => {
+      const fetchedWorkout = response.data;
+      setWorkout(fetchedWorkout);
+      setWorkoutName(fetchedWorkout.name);
+    });
   };
 
-  // Handle form submission for exercise editing
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Implement form submission logic here
+  const fetchAllExercises = () => {
+    api.get<Exercise[]>('/exercises').then((response) => {
+      setAllExercises(response.data);
+    });
+  };
+
+  const fetchExercises = () => {
+    api.get<Exercise[]>('/exercises').then((response) => {
+      const allExercises = response.data;
+      if (workout && workout.exerciseIds) {
+        const workoutExercises = allExercises.filter((exercise) =>
+          workout.exerciseIds.includes(exercise.id)
+        );
+        setExercises(workoutExercises);
+      } else {
+        setExercises([]);
+      }
+    });
   };
 
   const handleEditWorkoutName = () => {
-    setIsEditing(!isEditing);
+    setIsEditing(true);
   };
 
-  const handleSubmitWorkoutName = (event:any) => {
+  const handleSubmitWorkoutName = (event: React.FormEvent) => {
     event.preventDefault();
     setIsEditing(false);
-    // Perform any necessary logic with the updated workout name
+    // Update workout name logic here
   };
+
+  const handleAddExercise = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleExerciseSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = Number(event.target.value);
+    setSelectedExerciseId(selectedId);
+  };
+
+// ...import statements and component definition...
+
+const handleExerciseSubmit = (event: React.FormEvent) => {
+  event.preventDefault();
+  
+  if (selectedExerciseId && workout) {
+    const updatedWorkout = {
+      ...workout,
+      exerciseIds: [...workout.exerciseIds, selectedExerciseId]
+    };
+
+    api.put(`/workout/edit/${workout.id}`, updatedWorkout)
+      .then((response) => {
+        // Handle the successful response
+        const updatedWorkout = response.data;
+        setWorkout(updatedWorkout);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error('Failed to update workout:', error);
+      });
+  }
+
+  setSelectedExerciseId(null);
+  setIsModalOpen(false);
+};
+
+const handleRemove = (exerciseId: number) => {
+  if (workout) {
+    const updatedExerciseIds = workout.exerciseIds.filter(id => id !== exerciseId);
+    const updatedWorkout = {
+      ...workout,
+      exerciseIds: updatedExerciseIds
+    };
+
+    api.put(`/workout/edit/${workout.id}`, updatedWorkout)
+      .then((response) => {
+        // Handle the successful response
+        const updatedWorkout = response.data;
+        setWorkout(updatedWorkout);
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error('Failed to update workout:', error);
+      });
+  }
+};
+
+
+
+const handleRefresh = () => {
+  window.location.reload();
+};
+
+  useEffect(() => {
+    fetchWorkout();
+    fetchAllExercises(); // Fetch all exercises
+  }, [id]);
+
+  useEffect(() => {
+    if (workout) {
+      fetchExercises();
+    }
+  }, [workout]);
 
   return (
     <div className="edit-workout-container">
@@ -71,7 +179,7 @@ function EditWorkout() {
                 <td>{exercise.name}</td>
                 <td>{exercise.maxWeight}</td>
                 <td>
-                  <button onClick={() => handleDelete(exercise.id)}>Remove</button>
+                <button onClick={() => handleRemove(exercise.id)}>Remove</button>
                 </td>
               </tr>
             ))}
@@ -79,7 +187,27 @@ function EditWorkout() {
         </table>
       </div>
 
-      <button>Add Exercise</button>
+      <button onClick={handleAddExercise}>Add Exercise</button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Select an Exercise</h3>
+            <form onSubmit={handleExerciseSubmit}>
+              <select value={selectedExerciseId || ''} onChange={handleExerciseSelect}>
+                <option value="">Select an exercise</option>
+                {allExercises.map((exercise) => (
+                  <option key={exercise.id} value={exercise.id}>
+                    {exercise.name}
+                  </option>
+                ))}
+              </select>
+              <button type="submit">Save</button>
+              <button onClick={handleModalClose}>Cancel</button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
